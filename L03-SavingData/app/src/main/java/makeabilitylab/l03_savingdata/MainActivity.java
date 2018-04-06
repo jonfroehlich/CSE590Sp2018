c25e7cfc0e39b763ec7062d2bd33b494713423dc
Jon Froehlich <jonfroehlich@gmail.com>
2018-04-06 16:50:36 -0700

    Added (incomplete) saving data example for Android
    Finished basic UI. Need to hook up UI in MainActivity.

---
+++ L03-SavingData/app/src/main/java/makeabilitylab/l03_savingdata/MainActivity.java
@@ -0,0 +1,18 @@
+package makeabilitylab.l03_savingdata;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonSave(View view) {
+
+    }
+}
