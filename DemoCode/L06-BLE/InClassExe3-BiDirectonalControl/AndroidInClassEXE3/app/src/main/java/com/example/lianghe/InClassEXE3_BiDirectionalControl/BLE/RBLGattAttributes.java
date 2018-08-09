f1829e55240f0ca7b9eff452ce9b490f4657bc2c
Jon Froehlich <jonfroehlich@gmail.com>
2018-08-09 15:05:34 -0700

    Moved in-class exercise code to DemoCode folder

---
+++ DemoCode/L06-BLE/InClassExe3-BiDirectonalControl/AndroidInClassEXE3/app/src/main/java/com/example/lianghe/InClassEXE3_BiDirectionalControl/BLE/RBLGattAttributes.java
@@ -0,0 +1,29 @@
+package com.example.lianghe.InClassEXE3_BiDirectionalControl.BLE;
+
+import java.util.HashMap;
+
+/**
+ * This class includes a small subset of standard GATT attributes for
+ * demonstration purposes.
+ */
+public class RBLGattAttributes {
+    private static HashMap<String, String> attributes = new HashMap<String, String>();
+    public static String CLIENT_CHARACTERISTIC_CONFIG = "00002902-0000-1000-8000-00805f9b34fb";
+    public static String BLE_SHIELD_TX = "713d0003-503e-4c75-ba94-3148f18d941e";
+    public static String BLE_SHIELD_RX = "713d0002-503e-4c75-ba94-3148f18d941e";
+    public static String BLE_SHIELD_SERVICE = "713d0000-503e-4c75-ba94-3148f18d941e";
+
+    static {
+        // RBL Services.
+        attributes.put("713d0000-503e-4c75-ba94-3148f18d941e",
+                "BLE Shield Service");
+        // RBL Characteristics.
+        attributes.put(BLE_SHIELD_TX, "BLE Shield TX");
+        attributes.put(BLE_SHIELD_RX, "BLE Shield RX");
+    }
+
+    public static String lookup(String uuid, String defaultName) {
+        String name = attributes.get(uuid);
+        return name == null ? defaultName : name;
+    }
+}
---
+++ DemoCode/L06-BLE/InClassExe3-BiDirectonalControl/RedBearDuoInClassEXE3/ble_config.cpp
@@ -0,0 +1,52 @@
+#include "application.h"
+#include "ble_config.h"
+/******************************************************
+ *               Function Definitions
+ ******************************************************/
+/**
+ * @brief Connect handle.
+ *
+ * @param[in]  status   BLE_STATUS_CONNECTION_ERROR or BLE_STATUS_OK.
+ * @param[in]  handle   Connect handle.
+ *
+ * @retval None
+ */
+void deviceConnectedCallback(BLEStatus_t status, uint16_t handle) {
+  switch (status) {
+    case BLE_STATUS_OK:
+      Serial.println("Device connected!");
+      break;
+    default: break;
+  }
+}
+
+/**
+ * @brief Disconnect handle.
+ *
+ * @param[in]  handle   Connect handle.
+ *
+ * @retval None
+ */
+void deviceDisconnectedCallback(uint16_t handle) {
+  Serial.println("Disconnected.");
+}
+
+void configureBLE() {
+  // Register BLE callback functions
+  ble.onConnectedCallback(deviceConnectedCallback);
+  ble.onDisconnectedCallback(deviceDisconnectedCallback);
+
+  // Add GAP service and characteristics
+  ble.addService(BLE_UUID_GAP);
+  ble.addCharacteristic(BLE_UUID_GAP_CHARACTERISTIC_DEVICE_NAME, ATT_PROPERTY_READ|ATT_PROPERTY_WRITE, (uint8_t*)BLE_DEVICE_NAME, sizeof(BLE_DEVICE_NAME));
+  ble.addCharacteristic(BLE_UUID_GAP_CHARACTERISTIC_APPEARANCE, ATT_PROPERTY_READ, appearance, sizeof(appearance));
+  ble.addCharacteristic(BLE_UUID_GAP_CHARACTERISTIC_PPCP, ATT_PROPERTY_READ, conn_param, sizeof(conn_param));
+
+  // Add GATT service and characteristics
+  ble.addService(BLE_UUID_GATT);
+  ble.addCharacteristic(BLE_UUID_GATT_CHARACTERISTIC_SERVICE_CHANGED, ATT_PROPERTY_INDICATE, change, sizeof(change));
+
+  // Set BLE advertising parameters
+  ble.setAdvertisementParams(&adv_params);
+
+}
